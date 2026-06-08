4af649b8382b02b43437f020467ec5864c0e5e38
SandhiyaB2205 <sandhiyabalaji2205@gmail.com>
2026-06-08 19:28:55 +0530

    Add task deletion functionality

--- tasks.js
+++ tasks.js
@@ -32,13 +32,25 @@ function renderTasks() {
 
         if (task.completed) {
             li.style.textDecoration = "line-through";
-            li.style.color = "red";
+            li.style.color = "gray";
         }
 
         li.addEventListener("click", () => {
             task.completed = !task.completed;
             renderTasks();
         });
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener("click", (event) => {
+         
+
+            tasks.splice(index, 1);
+            renderTasks();
+        });
+
+        li.appendChild(deleteBtn);
 
         tasklist.appendChild(li);
     });
